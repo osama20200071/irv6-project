54aa7e1d2d01444ec8cf6412b1e410d20723d9b1
Osama <osamakaram118@gmail.com>
2025-12-06 18:36:22 +0200

    fix using useTransition

--- starter/transitions/src/App.jsx
+++ starter/transitions/src/App.jsx
@@ -1,31 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useTransition } from "react";
 import Score from "./Score";
 import getScore from "./getScore";
 
 export default function App() {
-  const [isPending, setIsPending] = useState(true);
+  const [isPending, startTransition] = useTransition();
   const [game, setGame] = useState(1);
   const [score, setScore] = useState({ home: "–", away: "–" });
 
   async function getNewScore(game) {
-    setIsPending(true);
-    setGame(game);
-    const newScore = await getScore(game);
-    setScore(newScore);
-    setIsPending(false);
+    startTransition(async () => {
+      // this code is deferred so it doesn't block the UI
+      // and to make sure we get the right last state update
+      // so react ignores updating with all the games and only updates with the last selected one.
+      // so now user can re-select if selected a wrong one and still get the right data.
+      const newScore = await getScore(game);
+      setScore(newScore);
+    });
   }
 
   useEffect(() => {
     getNewScore(game);
-  }, []);
+  }, [game]);
 
   return (
     <div className="app">
       <h1>Game {game}</h1>
       <select
-        disabled={isPending}
         onChange={(e) => {
-          getNewScore(e.target.value);
+          setGame(e.target.value);
         }}
       >
         <option value={1}>Game 1</option>
